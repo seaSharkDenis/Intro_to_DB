dccf3887386a14ceba2e3ee93d8a80a5d8e5d696
Denis Ndiritu <denisndiritu1@gmail.com>
2025-08-11 20:25:34 +0300

    Minor Ammendments

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE IF NOT EXISTS Order_Details (
